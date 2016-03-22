324aec2c4d63f442fbf66b9fb665c3208af0d8db
Christopher Goes <cwgoes@protonmail.com>
2016-03-21 21:01:50 -0400

    More framework code.

--- simulation/lib.py
+++ simulation/lib.py
@@ -1,6 +1,6 @@
 #> Python 3.5
 
-import collections, uuid, random
+import collections, uuid, random, time
 
 node  = collections.namedtuple('Node', ['ipV4Addr', 'darkNode', 'triedTable', 'newTable'])
 event = collections.namedtuple('Event', ['srcNode', 'destNode', 'eventType'])
@@ -9,9 +9,16 @@ event = collections.namedtuple('Event', ['srcNode', 'destNode', 'eventType'])
 
 randomIP = lambda: '.'.join([str(random.randint(0, 255)) for _ in range(4)])
 
-#> 
+#> React to events! May need additional arguments.
 
-def executeSimulation(numNodes = 5000, darkNodeProb = 0.5, simulationLength = 86400, timestep = 0.01):
+# Given a node and the global timestep, return a list of events.
+def react(node, globalTime):
+  # TODO implement me
+  return []
+
+def executeSimulation(numNodes = 50, darkNodeProb = 0.5, simulationLength = 86400, timestep = 0.1):
+
+  realStart = time.time()
   
   nodes = [node(
     ipV4Addr   = randomIP(),
@@ -20,9 +27,29 @@ def executeSimulation(numNodes = 5000, darkNodeProb = 0.5, simulationLength = 86
     newTable   = {}
     ) for _ in range(numNodes)]
 
+  eventLog = []
+
   globalTime = 0
   
   while globalTime < simulationLength:
+
     globalTime += timestep
 
-    # Do simulation things...
+    newEvents = []
+    for aNode in nodes:
+      reactions = react(aNode, globalTime)
+      newEvents += reactions
+      
+    for anEvent in newEvents:
+      # TODO handle the event, modifying node states as necessary. May need to include randomized latency.
+      # Maybe randomize some latency?
+      pass
+
+    eventLog += newEvents # Log these for later statistics, potentially?
+
+  realEnd = time.time()
+
+  # Simulation done! Return network state & event log.
+  print('Simulation complete in %.2f seconds.' % (realEnd - realStart))
+
+  return (nodes, eventLog)
