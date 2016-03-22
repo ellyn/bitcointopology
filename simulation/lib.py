6e422f0d3041750af2c3f23c16957f124a831c0d
Christopher Goes <cwgoes@protonmail.com>
2016-03-21 20:50:47 -0400

    A simulation function!

--- simulation/lib.py
+++ simulation/lib.py
@@ -8,3 +8,21 @@ event = collections.namedtuple('Event', ['srcNode', 'destNode', 'eventType'])
 #> Generate random IPv4 address
 
 randomIP = lambda: '.'.join([str(random.randint(0, 255)) for _ in range(4)])
+
+#> 
+
+def executeSimulation(numNodes = 5000, darkNodeProb = 0.5, simulationLength = 86400, timestep = 0.01):
+  
+  nodes = [node(
+    ipV4Addr   = randomIP(),
+    darkNode   = random.random() > darkNodeProb, 
+    triedTable = {},
+    newTable   = {}
+    ) for _ in range(numNodes)]
+
+  globalTime = 0
+  
+  while globalTime < simulationLength:
+    globalTime += timestep
+
+    # Do simulation things...
