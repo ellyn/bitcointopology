735ac555c0837333bae6b846b0481c32a73baa1c
Christopher Goes <cwgoes@protonmail.com>
2016-04-05 20:11:03 -0400

    Add argument parsing; add latency types to constants.

--- simulation/simulation2.py
+++ simulation/simulation2.py
@@ -1,8 +1,39 @@
-import sys
+#!/usr/bin/env python2.7
 
-def executeSimulation(numNodes = 800, simulationLength = 86400, latencyType = UNIFORM, darkNodeProb = 0.5, timestep = 0.1):
-    pass
+from constants import *
+import argparse
 
-if __name__ == "__main__":
-    try:
-        config = sys.argv[1]
+parser = argparse.ArgumentParser()
+parser.add_argument(
+  '-n',
+  '--nodes',
+  help='Run simulation with NODES number of nodes (default: 800)',
+  default=800,
+  type=int)
+parser.add_argument(
+  '-l',
+  '--length',
+  help='Run simulation for LENGTH seconds (default: 86400)',
+  default=86400,
+  type=int)
+parser.add_argument(
+  '-t',
+  '--latencyType',
+  help='Use LATENCYTYPE type of latency (default: 0 - uniform)',
+  default=UNIFORM,
+  type=int)
+parser.add_argument(
+  '-d',
+  '--darkNodeProb',
+  help='Set dark node probability (fractional) to DARKNODEPROB (default: 0.5)',
+  default=0.5,
+  type=float)
+
+
+def executeSimulation(numNodes, simulationLength, latencyType, darkNodeProb):
+  print('Simulation begun with {} nodes. Will run for {} seconds. Latency type: {}. Dark node probability: {}'.format(
+    numNodes, simulationLength, latencyType, darkNodeProb))
+
+if __name__ == '__main__':
+  args = parser.parse_args()
+  executeSimulation(args.nodes, args.length, args.latencyType, args.darkNodeProb)
